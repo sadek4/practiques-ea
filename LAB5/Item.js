72d99c01a61ff70dbe1ec267ef69af98a5d8fc60
Abdessadek <oscar.atiki@gmail.com>
2016-10-23 23:59:43 +0200

    Pràctica5

---
+++ LAB5/Item.js
@@ -0,0 +1,17 @@
+/**
+ * Created by boyander on 11/10/16.
+ */
+
+var Item = function Item(imagen, preu){
+    this.image = imagen;
+    this.price = 0.00;
+};
+
+// Get random price in range min, max
+Item.prototype.getPrice = function(){
+    var min = 100;
+    var max = 1000;
+    return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+exports.Item = Item;
